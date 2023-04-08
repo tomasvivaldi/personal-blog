b92848b89bb4aa1ecb0232c049d17c1df8d35e13
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-04-08 14:11:13 +0800

    Updated banner layout

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,50 +1,36 @@
 const Banner = () => {
   return (
     <div
-      className="flex flex-col justify-between  px-10 py-5 mb-10
+      className="flex flex-col justify-between px-5 sm:px-10 py-5 mb-10 rounded-xl
           lg:flex-row lg:space-x-5 bg-[--primary1-dark1] dark:bg-[--primary1-dark2] text-white"
     >
       <div className="">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-xl sm:text-4xl font-bold">
           Welcome to Tomas Vivaldi's Tech Hub
         </h1>
-        <h2 className="max-w-[90%] mt-5 md:mt-8 font-semibold text-lg md:text-xl">
-          Embrace the Future of{" "}
-          <span className="underline underline-offset-2 decoration-1 decoration-white">
-            Tech
-          </span>{" "}
-          <span className="underline underline-offset-2 decoration-1 decoration-white">
-            Programming
-          </span>{" "}
-          And{" "}
-          <span className="underline underline-offset-2 decoration-1 decoration-white">
-            Design
-          </span>{" "}
+        <h2 className="max-w-[90%] mt-2 sm:mt-5 md:mt-8 font-semibold text-sm sm:text-lg md:text-xl">
+          Embrace the Future of <span className="font-bold">Tech</span>
+          {", "}
+          <span className="font-bold">Programming</span> And{" "}
+          <span className="font-bold">Design</span>{" "}
         </h2>
       </div>
-      <div className="flex flex-col items-start cont w-fit">
+      <div className=" flex-col items-start cont w-fit hidden sm:flex">
         <p
-          className=" md:mt-2 py-2 mt-6 font-bold min-w-[365px]
+          className=" md:mt-2 py-2 mt-6 font-bold 
       "
         >
-          🤝 Need my services or want to
-          <span className="underline underline-offset-2 decoration-1 decoration-white">
-            {" "}
-            collaborate
-          </span>{" "}
-          ? 🤝
+          Need my services or want to
+          <span className=""> collaborate</span> ?
         </p>
-        <p>
-          Don't hesitate to{" "}
-          <a
-            href="mailto:contact@tomasvivaldi.com"
-            className="hover:underline hover:underline-offset-4 hover:decoration-[--primary1]  hover:decoration-3
-             font-extrabold"
-          >
-            Contact Me
-          </a>
-          !
-        </p>
+
+        <a
+          href="mailto:contact@tomasvivaldi.com"
+          className="hover:underline hover:underline-offset-4 hover:decoration-[--primary1]  hover:decoration-3
+             font-extrabold underline underline-offset-2 decoration-1 decoration-white"
+        >
+          Contact Me
+        </a>
       </div>
     </div>
   );
